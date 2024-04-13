16d27c49eb05687b01adc518633f620f0031f220
Reynan <reynannicolas99@gmail.com>
2024-04-13 22:58:36 +0800

    todo-rjs: third commit

--- src/components/NoSelectedProject.jsx
+++ src/components/NoSelectedProject.jsx
@@ -1,10 +1,13 @@
 import Button from './Button'
-const NoSelectedProject = ({handleAddCreateProject}) => {
+const NoSelectedProject = ({ handleAddCreateProject }) => {
     return (
-        <section>
-            No Selected Project
-
-        <Button buttonText="Create New" onClick={handleAddCreateProject}/>
+        <section className='no-selected-project section'>
+            <div className="div">
+                <p className="no-selected-section-title">
+                    Please select a project or create a new one.
+                </p>
+            </div>
+            <Button buttonText="Create New" onClick={handleAddCreateProject} />
         </section>
     )
 }
--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,24 +1,115 @@
-import { useRef } from "react"
+import { useRef, useState, useEffect } from "react"
 
 const ProjectForm = ({ handleCreateProject }) => {
 
     const projectNameRef = useRef()
     const projectDescriptionRef = useRef()
+    const [errorMessage, setErrorMessage] = useState({});
+
+
+    useEffect(() => {
+        function hasNoError(errorMessage) {
+            if (errorMessage.name === "" && errorMessage.desc === "") {
+                handleCreateProject({projectNameRef, projectDescriptionRef})
+            }
+        }
+        hasNoError(errorMessage)
+    }, [errorMessage, handleCreateProject])
+
+    function validateField() {
+        if (projectNameRef.current.value === "" || projectDescriptionRef.current.value === "") {
+            setErrorMessage(prev => {
+                return {
+                    ...prev,
+                    name: "Project name field is empty!",
+                    desc: "Project description field is empty",
+                }
+            })
+        } else if (projectNameRef.current.value.length < 8 || projectDescriptionRef.current.value.length < 12) {
+            setErrorMessage(prev => {
+                return {
+                    ...prev,
+                    name: "Project name should be at least 8 characters!",
+                    desc: "Project description should be at least 12 characters!",
+                }
+            })
+        } else {
+            setErrorMessage(() => {
+                return {
+                    name: "",
+                    desc: "",
+                }
+            })
+        }
+    }
+
+    function onSubmit(e) {
+        e.preventDefault()
+        validateField()
+    }
 
     return (
-        <form>
-            <input type="text" name="project_name" id="project-name" className="project-name" ref={projectNameRef} />
+        <section
+            className="section"
+        >
+            <h2
+                className="form-section-title"
+            >
+                New Project
+            </h2>
+            <form
+                onSubmit={onSubmit}
+                className={errorMessage.name !== "" ? "project-form has-error" : "project-form"}
+            >
+                <div className="form-group">
+                    <label
+                        htmlFor="project-name"
+                    >
+                        Project Name
+                    </label>
+                    <input
+                        type="text"
+                        name="project_name"
+                        id="project-name"
+                        className="project-name"
+                        ref={projectNameRef}
+                    />
+                    <span
+                        id="error-message-container"
+                    >
+                        {errorMessage.name != "" ? errorMessage.name : "Project name field is empty!"}
+                    </span>
+                </div>
 
-            <textarea name="project_description" id="project-description" className="project-description" ref={projectDescriptionRef}>
-            </textarea>
+                <div className="form-group">
+                    <label
+                        htmlFor="project-description"
+                    >
+                        Description
+                    </label>
+                    <textarea
+                        name="project_description"
+                        id="project-description"
+                        className="project-description"
+                        ref={projectDescriptionRef}
+                    >
+                    </textarea>
+                    <span
+                        id="error-message-container"
+                    >
+                        {errorMessage.desc !== "" ? errorMessage.desc : "Error!"}
+                    </span>
+                </div>
 
-            <button type="submit" id="submit-project" onClick={(e) => {
-                e.preventDefault()
-                handleCreateProject({projectNameRef, projectDescriptionRef})
-            }}>
-                Create
-            </button>
-        </form>
+                <button
+                    className="button"
+                    type="submit"
+                    id="submit-project"
+                >
+                    Create Project
+                </button>
+            </form>
+        </section>
     )
 }
 
